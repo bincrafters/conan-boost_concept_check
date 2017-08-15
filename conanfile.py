304d8e6a9c3c99384a8e2e8012433a76df95a16c
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:08 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,12 +3,15 @@ from conans import ConanFile, tools, os
 class BoostConcept_CheckConan(ConanFile):
     name = "Boost.Concept_Check"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/concept_check"
+    url = "https://github.com/bincrafters/conan-boost-concept_check"
+    source_url = "https://github.com/boostorg/concept_check"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
    
+    def package_info(self):
+        self.cpp_info.includedirs = []
+        
     def package_id(self):
         self.info.header_only()
         

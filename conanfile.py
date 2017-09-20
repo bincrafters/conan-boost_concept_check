c9a780b613c92ccb918991c3717dcfa7597aab17
Jerry W <jerrywiltse@gmail.com>
2017-09-20 15:31:27 -0400

    cleanup group members

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,9 @@
-from conans import ConanFile, tools, os
+from conans import ConanFile
+
 
 class BoostConcept_CheckConan(ConanFile):
     name = "Boost.Concept_Check"
     version = "1.65.1"
-    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-concept_check"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"

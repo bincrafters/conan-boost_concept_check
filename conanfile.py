bc90e2bfd8c77d2c4e13252af9cc1ec427936370
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:24 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostConcept_CheckConan(base.BoostBaseConan):
     name = "boost_concept_check"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_concept_check"
-    lib_short_names = ["concept_check"]
-    header_only_libs = ["concept_check"]
-    b2_requires = [
-        "boost_config",
-        "boost_preprocessor",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
